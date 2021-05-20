abf2c622ad0f4137c7e1e9fbce652719fa984bce
calarcon <malarcon@nimbussoft.net>
2021-05-20 12:38:00 -0500

    xD

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -96,7 +96,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
+                  href="https://www.instagram.com/vedderzeznick/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
